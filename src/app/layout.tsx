fbee191e04885b5dd4b81b1ddd942b2431589ad4
itsankitbhusal <ankitbhusal95@gmail.com>
2025-08-28 21:05:07 +0545

    removed link as it wasn't working, fixed library css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import AppQueryProvider from "@/providers/QueryProvider";
 import "./globals.css";
-import Head from "next/head";
+import "semantic-ui-css/semantic.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,12 +26,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
-        />
-      </Head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <AppQueryProvider>{children}</AppQueryProvider>
       </body>
